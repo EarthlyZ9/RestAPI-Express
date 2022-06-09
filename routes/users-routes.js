654de6475f25c50942da66a0ceb5a371409745ae
JS <linda2927@naver.com>
2022-06-09 16:18:23 +0900

    style: implementing prettier, eslint, and pre-commit hooks

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const usersControllers = require('../controllers/users-controller');
+const express = require("express");
+const usersControllers = require("../controllers/users-controller");
 
 const router = express.Router();
 
-router.get('/', usersControllers.getUsers);
-router.get('/:uid', usersControllers.getUserById);
-router.post('/signup', usersControllers.signup);
-router.post('/login', usersControllers.login);
+router.get("/", usersControllers.getUsers);
+router.get("/:uid", usersControllers.getUserById);
+router.post("/signup", usersControllers.signup);
+router.post("/login", usersControllers.login);
 
 module.exports = router;
