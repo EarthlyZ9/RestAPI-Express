8a13fac4509fb451365d69f05896a04234dc10e6
linda2927 <linda2927@naver.com>
2022-06-17 15:03:21 +0900

    feat: adding swagger - user model (#7)

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const mongoose = require("mongoose");
 const HttpError = require("./models/http-error");
 const placesRoutes = require("./routes/places-routes");
 const usersRoutes = require("./routes/users-routes");
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("./swagger/swagger-output.json");
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -17,6 +20,11 @@ app.use((req, res, next) => {
 
 app.use("/api/users", usersRoutes);
 app.use("/api/places", placesRoutes);
+app.use("/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerFile, { explorer: true })
+);
+
 app.use((req, res, next) => {
     throw new HttpError("Could not find this route.", 404);
 });
